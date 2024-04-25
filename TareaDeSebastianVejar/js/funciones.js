99be16a883708a60c3e485a7520ac760d63abca7
Sebast1an09889 <165072683+Sebast1an09889@users.noreply.github.com>
2024-04-25 17:16:06 -0400

    Add files via upload
    ultima, actualización del trabajo.

---
+++ TareaDeSebastianVejar/js/funciones.js
@@ -0,0 +1,4 @@
+function toggleForm() {
+    const nuevoFormulario = document.getElementById('nuevoFormulario');
+    nuevoFormulario.style.display = (nuevoFormulario.style.display === 'none') ? 'block' : 'none';
+}
